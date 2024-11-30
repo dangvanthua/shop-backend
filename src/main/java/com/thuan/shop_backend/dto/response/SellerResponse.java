a68020126ec9c38443f62095c7579552f155bc11
dangvanthua <thuandang.021102@gmail.com>
2024-12-01 01:09:24 +0700

    add seller

---
+++ src/main/java/com/thuan/shop_backend/dto/response/SellerResponse.java
@@ -0,0 +1,4 @@
+package com.thuan.shop_backend.dto.response;
+
+public class SellerResponse {
+}
---
+++ src/main/java/com/thuan/shop_backend/service/seller/ISellerService.java
@@ -0,0 +1,4 @@
+package com.thuan.shop_backend.service.seller;
+
+public interface ISellerSerivce {
+}
