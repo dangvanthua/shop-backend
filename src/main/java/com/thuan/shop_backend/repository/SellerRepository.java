15512d48ae92ea8fddaa0fafe1a0b20ab2a19716
dangvanthua <thuandang.021102@gmail.com>
2024-12-01 23:43:19 +0700

    fix error

--- src/main/java/com/thuan/shop_backend/repository/SellerRepository.java
+++ src/main/java/com/thuan/shop_backend/repository/SellerRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 public interface SellerRepository extends JpaRepository<Seller, Long> {
