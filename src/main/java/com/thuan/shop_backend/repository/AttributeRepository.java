308b3b574bafd1248a6cd4cdb70f6adbe1967daf
dangvanthua <thuandang.021102@gmail.com>
2024-12-08 15:33:41 +0700

    fix bug

--- src/main/java/com/thuan/shop_backend/repository/AttributeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thuan.shop_backend.repository;
-
-import com.thuan.shop_backend.entity.Attribute;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttributeRepository extends JpaRepository<Attribute, Long> {
-    Boolean existsByName(String name);
-}
